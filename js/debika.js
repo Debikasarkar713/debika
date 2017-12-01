0f82c39298b44192a7102235a7bc0d1dc74b3d10
Debika Sarkar <debikasarkar713@gmail.com>
2017-12-01 12:20:47 -0500

    hey

---
+++ js/debika.js
@@ -0,0 +1,9 @@
+$('#nav-menu-container a').click(function () {
+  var $href = $(this).attr('href');
+  console.log($href)
+  $('html,body').stop().animate({
+    scrollTop: $($href).offset().top
+  }, 1000);
+  return false;
+});
+
